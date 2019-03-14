e7f5f54436ae87061fd6af1cac214b01ca50a3c4
Hyunduk Shin <hyunduk0206@gmail.com>
2019-03-14 01:52:55 +0000

    minor

--- src/libs/driv.cpp
+++ src/libs/driv.cpp
@@ -1,8 +1,44 @@
 #include "driv.h"
 
-// Public
 namespace hvd190d_pi
 {
+    // private
+    enum Pin_spi { din = 19, sclk = 23, sync = 24 };
+    enum Pin_trig { x = 38, y = 40 };
+    enum Pin_misc { hv = 36 };
+    static void write_bit(int bit)
+    { 
+        digitalWrite(sclk, 1); 
+        digitalWrite(din, bit);
+        digitalWrite(sclk, 0); 
+    }
+    static void setup_pi()
+    {
+        wiringPiSetupPhys(); 
+        pinMode(din, OUTPUT); 
+        pinMode(sclk, OUTPUT); 
+        pinMode(sync, OUTPUT);
+        pinMode(x, OUTPUT);
+        pinMode(y, OUTPUT);
+        pinMode(hv, OUTPUT);
+    }
+    static void setup_dac()
+    {
+        write_spi(0x280001);
+        write_spi(0x380000);
+        write_spi(0x20000F);
+        write_spi(0x300000);
+    }
+    static void enable_hv()
+    {
+        digitalWrite(hv, 1);
+    }
+    static void disable_hv()
+    {
+        digitalWrite(hv, 0);
+    }
+    
+    // public
     clock_t t_start = 0;
     void t_reset()
     {
@@ -55,38 +91,4 @@ namespace hvd190d_pi
         disable_hv();
     }
 
-    // Private
-
-
-    void write_bit(int bit)
-    { 
-        digitalWrite(sclk, 1); 
-        digitalWrite(din, bit);
-        digitalWrite(sclk, 0); 
-    }
-    void setup_pi()
-    {
-        wiringPiSetupPhys(); 
-        pinMode(din, OUTPUT); 
-        pinMode(sclk, OUTPUT); 
-        pinMode(sync, OUTPUT);
-        pinMode(x, OUTPUT);
-        pinMode(y, OUTPUT);
-        pinMode(hv, OUTPUT);
-    }
-    void setup_dac()
-    {
-        write_spi(0x280001);
-        write_spi(0x380000);
-        write_spi(0x20000F);
-        write_spi(0x300000);
-    }
-    void enable_hv()
-    {
-        digitalWrite(hv, 1);
-    }
-    void disable_hv()
-    {
-        digitalWrite(hv, 0);
-    }
 }
--- src/libs/driv.h
+++ src/libs/driv.h
@@ -2,11 +2,11 @@
 #define HVD190D_PI_WRITE_SPI_DRIV_H
 
 #include <wiringPi.h>
-#include <iostream> // For the inline functions
+#include <iostream> // for the inline functions
 
 namespace hvd190d_pi
 {
-    int const delay_loop_pi_ns = 1500;
+    int const delay_loop_pi_ns = 1500; // while loop delay - spi delay
 
     extern clock_t t_start;
     inline int t_lapsed()
@@ -18,9 +18,6 @@ namespace hvd190d_pi
         return 0x100000 | ( ( (0x000000 | (ch-1)) << 16) | v); 
     }
 
-    enum Pin_spi { din = 19, sclk = 23, sync = 24 };
-    enum Pin_trig { x = 38, y = 40 };
-    enum Pin_misc { hv = 36 };
     void t_reset();
     void write_spi(unsigned long bits);
     void write_spi(unsigned long bits_p, unsigned long bits_n);
@@ -30,12 +27,6 @@ namespace hvd190d_pi
     void write_trig_y(int signal);
     void initialize();
     void terminate();
-    
-    void write_bit(int bit);
-    void setup_pi();
-    void setup_dac();
-    void enable_hv();
-    void disable_hv();
 }
 
 #endif
