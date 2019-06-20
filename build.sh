7fb29a535d29eb3b194c4b92ff973861835f17c0
Hyunduk Shin <hyunduk0206@gmail.com>
2019-06-20 09:31:05 +0100

    mns

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ cmake ..
 
 make
 
-./demo ./../src/apps/wf_trig.csv
+./demo ./wf4.csv
+#./demo ./../src/apps/wf_trig.csv
 #./demo ./../src/apps/wf.csv
 #./demo ./../src/apps/wf_max_speed_trig.csv
 #./demo ./../src/apps/wf_max_speed.csv
--- src/apps/main.cpp
+++ src/apps/main.cpp
@@ -5,158 +5,155 @@
 #include <thread> // std::thread(), std::this_thread::sleep_for
 #include <chrono> // std::chrono::seconds
 #include <atomic> // atomic function
-//#include <mutex> // mutex
 
 #include <sstream> // <string>; std::stringstream
 
-std::vector<unsigned long> t_us;
-std::vector<unsigned long> cmd_wf_p;
-std::vector<unsigned long> cmd_wf_n;
+#include <fstream> //csv process
 
-//std::mutex mtx;
-
-void drive_hvd190d(std::atomic<bool>& ref_is_running, std::atomic<bool>& ref_is_on_thread_driv)
+struct buffer
 {
-    while (ref_is_on_thread_driv)
+    std::vector<unsigned long> t_us;
+    std::vector<unsigned long> cmd_wf_p;
+    std::vector<unsigned long> cmd_wf_n;
+};
+
+buffer wf1, wf2, wf3;
+
+void driv(buffer wf)
+{
+    hvd190d_pi::t_reset(); // take A
+    for (int i = 0; i < wf.t_us.size(); i++ ) // take B, A + B = ~ 1.2 us 
     {
-//        std::lock_guard<std::mutex> lock(mtx);
-//        mtx.lock();
-        while (ref_is_running)
-        {
-            hvd190d_pi::t_reset(); // take A
-            for (int i = 0; i < t_us.size(); i++ ) // take B, A + B = ~ 1.2 us 
-            {
-                while ( hvd190d_pi::t_lapsed() < t_us[i] ); // take ~1.4 us
-                hvd190d_pi::write_spi(cmd_wf_p[i], cmd_wf_n[i]);
-            }
-        }
-//        mtx.unlock();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        while ( hvd190d_pi::t_lapsed() < wf.t_us[i] ); // take ~1.4 us
+        hvd190d_pi::write_spi(wf.cmd_wf_p[i], wf.cmd_wf_n[i]);
     }
 }
 
-int main(int args_len, char * args[]) 
+void driv_arb(std::vector<int> p_csv)
+{
+    enum csv_col { ch, t_us, dac};
+    // column 0 : driver output channel
+    // column 1 : time in microseconds
+    // column 2 : 16-bit DAC
+	
+    hvd190d_pi::t_reset();
+    for (unsigned int k = 0; k < p_csv.size(); k += 3) 
+	{ 
+        while ( hvd190d_pi::t_lapsed() < p_csv[k + t_us] );
+        hvd190d_pi::write_spi(p_csv[k + ch], p_csv[k + dac]);
+	} 
+}
+
+//////////////////// import csv data //////////////////// 
+using namespace std; 
+template <class T> 
+class csv_istream_iterator: public iterator<input_iterator_tag, T> 
+{ 
+	istream * _input; 
+	char _delim; 
+	string _value; 
+public: 
+	csv_istream_iterator( char delim = ',' ): _input( 0 ), _delim( delim ) {} 
+	csv_istream_iterator( istream & in, char delim = ',' ): _input( &in ), _delim( delim ) { ++*this; } 
+
+	const T operator *() const 
+    { 
+		istringstream ss( _value ); 
+		T value; 
+		ss >> value; 
+		return value; 
+	}
+ 
+	istream & operator ++() 
+    { 
+		if( !( getline( *_input, _value, _delim ) ) ) 
+		{ 
+			_input = 0; 
+		} 
+		return *_input; 
+	}
+ 
+	bool operator !=( const csv_istream_iterator & rhs ) const 
+    { 
+		return _input != rhs._input; 
+	} 
+}; 
+
+template <> 
+const string csv_istream_iterator<string>::operator *() const 
+{ 
+	return _value; 
+} 
+
+vector<int> readData(char filename[]) 
+{ 
+    vector<int> data; 
+    { // test for integers 
+        ifstream fin( filename ); 
+    	if( fin ) 
+    	{ 
+    		copy( csv_istream_iterator<int>( fin ), 
+    			csv_istream_iterator<int>(), 
+    			back_inserter(data) );
+			fin.close(); 
+		} 
+	} 
+    return data; 
+} 
+
+int main(int args_len, char * args[]) // release mode!
 {
     // start driver
     std::cout << "on" << std::endl;
     hvd190d_pi::initialize();
 
-    // create waveform class
+    //////////////////// waveform generation 
     hvd190d_pi::wf wf_main;
     wf_main.set_is_diff_on(true);
     wf_main.set_is_x_on(true);
     wf_main.set_is_y_on(true);
-	wf_main.set_param_wf(0, 25400, 100000, 1, 0.1, 60, 60, 0, 0);
-	wf_main.set_param_wf(1, 25400, 100000, 3, 400, 10, 10, 0, 0);
+    
+    // mode 1
+	wf_main.set_param_wf(0, 25400, 100000, 2, 0.3, -60, 60, 0, 0); // y-axis
+	wf_main.set_param_wf(1, 25400, 100000, 0, 60, 60, 60, 0, 0); // x-axis
 	wf_main.run_wf_differential();
-	t_us = wf_main._sorted_cmd_wf.t_us;
-	cmd_wf_p = wf_main._sorted_cmd_wf.cmd_wf_p;
-	cmd_wf_n = wf_main._sorted_cmd_wf.cmd_wf_n;
+	wf1.t_us = wf_main._sorted_cmd_wf.t_us;
+	wf1.cmd_wf_p = wf_main._sorted_cmd_wf.cmd_wf_p;
+	wf1.cmd_wf_n = wf_main._sorted_cmd_wf.cmd_wf_n;
 
-    // create thread
-    std::atomic<bool> is_running { false }; 
-    std::atomic<bool> is_on_thread_driv { true }; 
-    std::thread t_driv;
-    // start thread
-    t_driv = std::thread(drive_hvd190d, std::ref(is_running), std::ref(is_on_thread_driv));
-//    t_driv = std::thread(drive_hvd190d, std::ref(wf_main), std::ref(is_running), std::ref(is_on_thread_driv));
+    // mode 2
+	wf_main.set_param_wf(0, 25400, 100000, 0, 60, 60, 60, 0, 0); // y-axis
+	wf_main.set_param_wf(1, 25400, 100000, 0, 60.1, 60, 60, 0, 0); // x-axis
+	wf_main.run_wf_differential();
+	wf2.t_us = wf_main._sorted_cmd_wf.t_us;
+	wf2.cmd_wf_p = wf_main._sorted_cmd_wf.cmd_wf_p;
+	wf2.cmd_wf_n = wf_main._sorted_cmd_wf.cmd_wf_n;
 
-////////////////////////////////////////
+    // mode 3
+	wf_main.set_param_wf(0, 25400, 100000, 1, 0.4, 60, 60, 0, 0); // y-axis
+	wf_main.set_param_wf(1, 25400, 100000, 1, 0.1, 60, 60, 0, 0); // x-axis
+	wf_main.run_wf_differential();
+	wf3.t_us = wf_main._sorted_cmd_wf.t_us;
+	wf3.cmd_wf_p = wf_main._sorted_cmd_wf.cmd_wf_p;
+	wf3.cmd_wf_n = wf_main._sorted_cmd_wf.cmd_wf_n;
 
-    // start server
-    rpi::init_tcp_server();
-    while ( rpi::data_tcp[0] != '4' ) 
+    // mode 4
+    std::vector<int> wf4 = readData(args[1]); 
+
+
+    //////////////////// drive 
+    while(1)
     {
-        rpi::run_tcp_server();
-        while ( rpi::data_tcp[0] != '3' && rpi::data_tcp[0] != '4' ) 
-        {
-            rpi::get_data_tcp_server();
-            if (rpi::data_tcp[0] >= '0' && rpi::data_tcp[0] <= '4')
-            {
-                if (rpi::data_tcp[0] == '0') // stop 
-                {
-                    rpi::send_data_tcp_server((unsigned char *)"STOP");
-					is_running = false;
-                }
-                else if (rpi::data_tcp[0] == '1') // run
-                {
-                    rpi::send_data_tcp_server((unsigned char *)"START");
-					is_running = true;
-                }
-                else if (rpi::data_tcp[0] == '2') // setting
-                {
+        driv(wf1);
+        driv(wf1);
+        driv(wf1);
 
-                    std::string data_s(reinterpret_cast<char*>(rpi::data_tcp));
-                    std::vector<double> data_v;
+        driv(wf2);
 
-                    std::stringstream data_ss(data_s);
+        driv(wf3);
 
-                    double i_ss;
-
-                    while (data_ss >> i_ss)
-                    {
-                        data_v.push_back(i_ss);
-
-                        if (data_ss.peek() == ',' || data_ss.peek() == ' ');
-                        {
-                            data_ss.ignore();
-                        }
-                    }
-                    if (data_v.size() == 15)
-                    {
-                        rpi::send_data_tcp_server((unsigned char *)"SETTING");
-                        is_running = false;
-
-                        int fs[2], wf[2];
-                        double freq[2], amp[2], offset[2], phase[2], width[2]; 
-                        
-                        for (int i = 0; i < 2; i++)
-                        {
-                            fs[i] = static_cast<int>(data_v.at(1 + i*7));
-                            wf[i] = static_cast<int>(data_v.at(2 + i*7));
-                            freq[i] = data_v.at(3 + i*7);
-                            amp[i] = data_v.at(4 + i*7);
-                            offset[i] = data_v.at(5 + i*7);
-                            phase[i] = data_v.at(6 + i*7);
-                            width[i] = data_v.at(7 + i*7);
-                        }
-
-                        wf_main.set_param_wf(0, fs[0], 100000, wf[0], freq[0], amp[0], offset[0], phase[0], width[0]);
-                        wf_main.set_param_wf(1, fs[0], 100000, wf[1], freq[1], amp[1], offset[1], phase[1], width[1]);
-                        wf_main.run_wf_differential();
-                        // If the drive thread is running, the code below means reading and writing at the same time and
-                        // causes undefined behavior.
-                        t_us = wf_main._sorted_cmd_wf.t_us;
-                        cmd_wf_p = wf_main._sorted_cmd_wf.cmd_wf_p;
-                        cmd_wf_n = wf_main._sorted_cmd_wf.cmd_wf_n;
-                    }
-                    else
-                    {
-                        rpi::send_data_tcp_server((unsigned char *)"INVALID");
-                    }
-
-                }
-            }
-            else
-            {
-                rpi::send_data_tcp_server((unsigned char *)"INVALID");
-            }
-        }
-
-		// end tcp server
-		rpi::terminate_tcp_server();
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    }
-
-////////////////////////////////////////
-
-    // terminate therad
-    is_running = false;
-    is_on_thread_driv= false;
-    if (t_driv.joinable())
-    {
-        t_driv.join();
+        driv_arb(wf4);
+        driv_arb(wf4);
     }
 
     // end driver
